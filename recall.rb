61845b6e1c4ace29b24c8fc91ca9da3828982c35
Pavel Gaimovich <pavels.gaimovics@gmail.com>
2015-10-31 11:22:14 +0200

    Adding Flash.

--- recall.rb
+++ recall.rb
@@ -1,5 +1,11 @@
 require 'sinatra'
 require 'data_mapper'
+require 'sinatra/flash'
+
+enable :sessions
+
+SITE_TITLE = "Recall"
+SITE_DESCRIPTION = "'cause you're too busy to remember"
 
 DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/recall.db")
  
@@ -19,9 +25,11 @@ helpers do
     alias_method :h, :escape_html
 end
 
+#home page
 get '/' do
   @notes = Note.all :order => :id.desc
   @title = 'All Notes'
+  flash[:error] = 'No notes found. Add your first below.' if @notes.empty?
   erb :home
 end
 
@@ -31,13 +39,29 @@ post '/' do
   n.created_at = Time.now
   n.updated_at = Time.now
   n.save
+  if n.save
+    flash[:notice] = 'Note created successfully.'
+  else
+    flash[:error] = 'Failed to save note.'
+  end
   redirect '/'
 end
 
+get '/rss.xml' do
+    @notes = Note.all :order => :id.desc
+    builder :rss
+end
+
+#Editing note
 get '/:id' do
   @note = Note.get params[:id]
   @title = "Edit note ##{params[:id]}"
-  erb :edit
+  if @note 
+    erb :edit
+  else
+    flash[:error] = "Can't find that node"
+    redirect '/'
+  end
 end
 
 put '/:id' do
@@ -45,14 +69,24 @@ put '/:id' do
   n.content = params[:content]
   n.complete = params[:complete] ? 1 : 0
   n.updated_at = Time.now
-  n.save
+  if n.save
+    flash[:notice] = 'Note updated successfully'
+  else
+    flash[:error] = 'Failed to save note'
+  end
   redirect '/'
 end
 
+#Deleting note
 get '/:id/delete' do
   @note = Note.get params[:id]
   @title = "Confirm deletion of note ##{params[:id]}"
-  erb :delete
+  if @note
+    erb :delete
+  else 
+    flash[:error] = "Can't find that node"
+    redirect '/'
+  end
 end
 
 delete '/:id' do
@@ -63,8 +97,13 @@ end
 
 get '/:id/complete' do
   n = Note.get params[:id]
+  flash[:error] = "Can't find that note" unless n
   n.complete = n.complete ? 0 : 1 # flip it
   n.updated_at = Time.now
-  n.save
+  if n.save
+    flash[:notice] = 'Note marked as complete'
+  else
+    flash[:error] = 'Error marking note as complete'
+  end  
   redirect '/'
 end
